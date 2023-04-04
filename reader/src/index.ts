13050dd13e2a3975a9440f0e29d95890f302b37b
Johannes Fritsch <fritsch@zoom7.de>
2023-04-04 11:04:47 +0200

    Allow to skip reader

--- reader/src/index.ts
+++ reader/src/index.ts
@@ -6,10 +6,15 @@ const nfc = new NFC();
 let readerIndex = 0;
 nfc.on("reader", async (reader: Reader) => {
   console.log(`[global] ${reader.reader.name} device attached`);
-  createReaderHandler(reader, {
-    apiUrl: process.env.API_URL || "http://localhost:4000/graphql",
-    readerToken: process.env[`READER_TOKEN${readerIndex++}`] || '',
-  });  
+  const readerToken = process.env[`READER_TOKEN${readerIndex++}`];
+
+  if (readerToken) {
+    console.log(`[global] ${reader.reader.name} has a token, connecting to API`);
+    createReaderHandler(reader, {
+      apiUrl: process.env.API_URL || "http://localhost:4000/graphql",
+      readerToken,
+    });
+  }
 });
 
 nfc.on("error", (err: any) => {
